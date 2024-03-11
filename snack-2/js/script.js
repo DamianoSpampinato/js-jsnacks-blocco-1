da0238011c1c906ac7c675e44b6b0c4be2ced89e
DamianoSpampinato <ghgg585@gmail.com>
2024-03-11 16:18:00 +0100

    fix userSum value

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,7 +1,8 @@
 //- Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //  Il programma stampa la somma di tutti i numeri inseriti.
 
-let userSum;
+let userSum = 0;
 for(let i = 0; i < 5; i++){
-
+    userSum += parseInt(prompt('inserisci un numero'));
+    console.log(userSum);
 }
