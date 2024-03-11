c978d135e9cdad17a321d2b3621de4fcd70da7b4
DamianoSpampinato <ghgg585@gmail.com>
2024-03-11 16:43:49 +0100

    feat add let sumNotGpt + cycle for

---
+++ snack-4/js/script.js
@@ -0,0 +1,10 @@
+//- Calcola la somma e la media dei primi 10 numeri.
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numberArray)
+let sumNotGpt = 0;
+for(let i = 0; i < numberArray.length; i++){
+    sumNotGpt += numberArray[i];
+    console.log(sumNotGpt);
+
+
+}
