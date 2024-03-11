9eed616a6be2a16d86f1e89348ec0876bbf1dc4a
DamianoSpampinato <ghgg585@gmail.com>
2024-03-11 16:10:53 +0100

    feat: add let userSum + for cylce

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,3 +1,7 @@
 //- Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //  Il programma stampa la somma di tutti i numeri inseriti.
 
+let userSum;
+for(let i = 0; i < 5; i++){
+
+}
