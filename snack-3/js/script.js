2456806a5d043ea72b822d676a0008896a85bd5e
DamianoSpampinato <ghgg585@gmail.com>
2024-03-11 16:30:17 +0100

    feat add for cycle

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,3 +1,11 @@
 //- Crea un array vuoto.
 //  Chiedi per 6 volte all’utente di inserire un numero,
 //  se è dispari inseriscilo nell’array.
+const emptyArray = [];
+for(let i = 0; i < 6; i++){
+    let userNumber = parseInt(prompt('inserisci numero preferibilmente dispari perchè se no lo scarto'))
+    if(userNumber % 2 !== 0){
+        emptyArray.push(userNumber);
+    }
+}
+console.log(emptyArray);
