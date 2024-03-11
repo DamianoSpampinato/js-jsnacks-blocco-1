855f674ff1e2b20fa8ee250f4f143e5490c13c8b
DamianoSpampinato <ghgg585@gmail.com>
2024-03-11 15:50:10 +0100

    Feat add const numberArray

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 //- Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati 
 //(inserire il numero nell'array solo se non è già presente).
 
+const numberArray = [15, 10, 19, 21, 22, 13];
+
